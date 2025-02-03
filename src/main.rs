514a44b7a047b2b15669c1d39bef3d4671e12407
Artem Sergeev <twin7366@mail.ru>
2025-02-03 21:13:43 +0300

    Implemented a simple server + config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello");
+use crate::setting::settings::Settings;
+use axum::http::{StatusCode, Uri};
+use axum::Router;
+use tokio::net::TcpListener;
+use crate::http::api::*;
+
+mod http;
+mod repository;
+mod service;
+mod setting;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let settings = Settings::new("config.yaml")?;
+
+    let address = &format!("{}:{}", settings.server.host, settings.server.port);
+
+    let listener = TcpListener::bind(address).await?;
+
+    axum::serve(listener, routes()).await?;
+
+    Ok(())
+}
+
+fn routes() -> Router {
+    let all_routes = Router::new().nest("/v1", system_routes()).nest("/v1", test_routes());
+
+    Router::new().nest("/api", all_routes).fallback(fallback)
+}
+
+async fn fallback(uri: Uri) -> (StatusCode, String) {
+    (StatusCode::NOT_FOUND, format!("Handler for route '{uri}' not found!"))
 }
---
+++ src/setting/settings.rs
@@ -0,0 +1,31 @@
+use config::Config;
+use log::{log, Level};
+use serde::{Deserialize, Serialize};
+use std::path::Path;
+
+#[derive(Debug, Deserialize, Serialize, Default)]
+pub struct Server {
+    pub host: String,
+    pub port: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, Default)]
+pub struct Settings {
+    pub server: Server,
+}
+
+impl Settings {
+    pub fn new(location: &str) -> anyhow::Result<Self> {
+        let mut builder = Config::builder();
+
+        if Path::new(location).exists() {
+            builder = builder.add_source(config::File::with_name(location));
+        } else {
+            log!(Level::Warn, "Configuration file not found");
+        }
+
+        let settings = builder.build()?.try_deserialize()?;
+
+        Ok(settings)
+    }
+}
